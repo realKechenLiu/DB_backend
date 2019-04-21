a6a6780597c4831b097817d7c59f7ae99f292ae8
Victor <1412621@mail.nankai.edu.cn>
2019-04-20 23:56:15 -0400

    a

--- src/main/java/com/kechen/controller/MyController.java
+++ src/main/java/com/kechen/controller/MyController.java
@@ -10,11 +10,9 @@ import com.kechen.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.ws.rs.Path;
-import java.util.HashSet;
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -68,14 +66,14 @@ public class MyController {
     }
 
 
-    @RequestMapping(value = "/api/problem/delete",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/problem/delete",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
     @CrossOrigin
-    public int delete(@RequestBody Map<String,Object>[] maps){
-        int id = (int)maps[0].get("id");
-        String description = (String)maps[0].get("description");
-        String title = (String)maps[0].get("title");
-        int difficulty = (int)maps[0].get("difficulty");
-        String tag = (String)maps[0].get("tag");
+    public int delete(@RequestBody Map<String,Object> maps){
+        int id = (int)maps.get("id");
+        String description = (String)maps.get("description");
+        String title = (String)maps.get("title");
+        int difficulty = (int)maps.get("difficulty");
+        String tag = (String)maps.get("tag");
         Problem p = problemRepository.findByProblemId(id);
         if(p==null) return 404;
 
@@ -109,14 +107,14 @@ public class MyController {
 //            return problemService.Insert(new Problem(id,description,title,difficulty,tag))?200:400;
 //    }
 
-    @RequestMapping(value = "/api/problem/insert",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/problem/insert",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
     @CrossOrigin
-    public int insertProblem(@RequestBody Map<String,Object>[] maps){
-        int problemId = (int) maps[0].get("problemId");
-        String description = (String) maps[0].get("description");
-        String title = (String) maps[0].get("title");
-        int difficulty = (int) maps[0].get("difficulty");
-        String tag = (String) maps[0].get("tag");
+    public int insertProblem(@RequestBody Map<String,Object> maps){
+        int problemId = (int) maps.get("problemId");
+        String description = (String) maps.get("description");
+        String title = (String) maps.get("title");
+        int difficulty = (int) maps.get("difficulty");
+        String tag = (String) maps.get("tag");
         if(problemId == -1)
             return problemService.Insert(new Problem(description,title,difficulty,tag))?200:400;
         else
@@ -133,26 +131,26 @@ public class MyController {
 //            return 400;
 //    }
 
-    @RequestMapping(value = "/api/user/signup",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/user/signup",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
     @CrossOrigin
-    public int signup(@RequestBody Map<String,Object>[] maps){
-        String userName = (String) maps[0].get("userName");
-        String password = (String) maps[0].get("password");
-        String email = (String) maps[0].get("email");
-        int isAdmin = (int)maps[0].get("isAdmin");
+    public int signup(@RequestBody Map<String,Object> maps){
+        String userName = (String) maps.get("userName");
+        String password = (String) maps.get("password");
+        String email = (String) maps.get("email");
+        int isAdmin = (int)maps.get("isAdmin");
         if(userService.register(userName,email,password,isAdmin))
             return 200;
         else
             return 400;
     }
 
-    @RequestMapping(value = "/api/problem/submission",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/problem/submission",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
     @CrossOrigin
-    public int update(@RequestBody Map<String,Object>[] maps){
-        int userId = (int)maps[0].get("user_id");
-        int problemId = (int)maps[0].get("problem_id");
-        Code code = (Code)maps[0].get("code");
-        Note note = (Note)maps[0].get("note");
+    public int update(@RequestBody Map<String,Object> maps){
+        int userId = (int)maps.get("user_id");
+        int problemId = (int)maps.get("problem_id");
+        Code code = (Code)maps.get("code");
+        Note note = (Note)maps.get("note");
 
         codeRepository.save(code);
         noteRepository.save(note);
@@ -171,12 +169,16 @@ public class MyController {
         return 400;
     }
 
-    @RequestMapping(value = "/api/problem",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/problem",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
     @CrossOrigin
-    public ResponseEntity<Iterable<ProblemCode>> getOneProblem(@RequestBody Map<String,Object>[] maps){
-        int userId = (int)maps[0].get("user_id");
-        int problemId = (int)maps[0].get("problem_id");
+    public ResponseEntity<Iterable<ProblemCode>> getOneProblem(@RequestBody Map<String,Object> maps){
 
+//        System.out.println(maps);
+        int userId = Integer.parseInt((String)maps.get("user_id"));
+        int problemId = Integer.parseInt((String)maps.get("problem_id"));
+
+//        System.out.println("user is "+userId);
+//        System.out.println("problem is "+problemId);
         User user = userService.findById(userId);
         ProblemCode pc = new ProblemCode();
 
@@ -200,41 +202,45 @@ public class MyController {
         return new ResponseEntity<Iterable<ProblemCode>>(list, HttpStatus.OK);
     }
 
-//    @RequestMapping(value = "/api/problem/{userId}/{problemId}")
-//    @CrossOrigin
-//    public ResponseEntity<Iterable<ProblemCode>> getOneProblemq(@PathVariable int userId, @PathVariable int problemId){
-//
-//        User user = userService.findById(userId);
-//        ProblemCode pc = new ProblemCode();
-//
-//        for(Problem_Code p:user.getPcSet()){
-//            System.out.println(p.getProblem().getId()+" "+problemId);
-//            if(p.getProblem().getId() == problemId){
-//                pc.problemId = p.getProblem().getProblemId();
-//                pc.description = p.getProblem().getDescription();
-//                pc.difficulty = p.getProblem().getDifficulty();
-//                pc.tag = p.getProblem().getTag();
-//                pc.title = p.getProblem().getTitle();
-//                for(Problem_Code code:p.getProblem().getPcodeSet()){
-//                    pc.codeList.add(code.getCode());
-//                }
-//                for(Problem_Company pcom:p.getProblem().getPcomSet()){
-//                    pc.companyList.add(pcom.getCompany().getCompanyName());
-//                }
-//            }
-//        }
-//        List<ProblemCode> list = new LinkedList<>();
-//        list.add(pc);
-//        return new ResponseEntity<Iterable<ProblemCode>>(list, HttpStatus.OK);
-//    }
-
-    @RequestMapping(value = "/api/login",method = RequestMethod.POST)
+    @RequestMapping(value = "/api/problem/{userId}/{problemId}")
     @CrossOrigin
-    public String login(@RequestBody Map<String,Object>[] maps){
-        String email = (String) maps[0].get("email");
-        String password = (String) maps[0].get("password");
+    public ResponseEntity<Iterable<ProblemCode>> getOneProblemq(@PathVariable int userId, @PathVariable int problemId){
+
+        User user = userService.findById(userId);
+        ProblemCode pc = new ProblemCode();
+
+        for(Problem_Code p:user.getPcSet()){
+//            System.out.println(p.getProblem().getId()+" "+problemId);
+            if(p.getProblem().getId() == problemId){
+                pc.problemId = p.getProblem().getProblemId();
+                pc.description = p.getProblem().getDescription();
+                pc.difficulty = p.getProblem().getDifficulty();
+                pc.tag = p.getProblem().getTag();
+                pc.title = p.getProblem().getTitle();
+                for(Problem_Code code:p.getProblem().getPcodeSet()){
+                    pc.codeList.add(code.getCode());
+                }
+                for(Problem_Company pcom:p.getProblem().getPcomSet()){
+                    pc.companyList.add(pcom.getCompany().getCompanyName());
+                }
+            }
+        }
+        List<ProblemCode> list = new LinkedList<>();
+        list.add(pc);
+        return new ResponseEntity<Iterable<ProblemCode>>(list, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/login",method = RequestMethod.POST,consumes="application/json;charset=UTF-8")
+    @CrossOrigin
+//    public ResponseEntity<Iterable<ReturnType>> login(@RequestBody Map<String,Object> maps){
+    public String login(@RequestBody Map<String,Object> maps){
+        String email = (String) maps.get("email");
+        String password = (String) maps.get("password");
         User user = userRepository.findByEmailAndPassword(email,password);
-        return "{\"status\":"+userService.loginByEmail(email,password)+",\n\"admin\""+user!=null?((user.getIsAdmin()==1)+""):false+"}";
+        List<ReturnType> list = new LinkedList<>();
+        list.add(new ReturnType(userService.loginByEmail(email,password),user!=null?((user.getIsAdmin()==1)):false));
+//        return new ResponseEntity<Iterable<ReturnType>>(list, HttpStatus.OK);
+        return "{\"status\":"+userService.loginByEmail(email,password)+",\n\"admin\":"+(user!=null?((user.getIsAdmin()==1)):false)+"}";
     }
 
     @RequestMapping("/api/problems")
@@ -286,7 +292,7 @@ public class MyController {
     }
 }
 
-class ReturnType{
+class ReturnType implements Serializable{
     int status;
     boolean isAdmin;
 
